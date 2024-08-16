fc8f97cecc66a9fcf84f08d236a325d25212d8ff
Jonanda27 <jonandapantas@gmail.com>
2024-08-16 18:06:59 +0700

    Update toko_model.dart

--- frontend/lib/Model/toko_model.dart
+++ frontend/lib/Model/toko_model.dart
@@ -1,13 +1,16 @@
 class TokoModel {
   final int id;
   final int userId;
-  final String fotoProfileToko;
+  final String fotoProfileToko; // Changed to nullable to match `null` in JSON
   final String namaToko;
   final String kategoriToko;
   final String alamatToko;
   final String nomorTeleponToko;
   final String emailToko;
   final String deskripsiToko;
+  final String provinsiToko; // Added to match JSON
+  final String kotaToko; // Added to match JSON
+  
 
   TokoModel({
     required this.id,
@@ -19,6 +22,8 @@ class TokoModel {
     required this.nomorTeleponToko,
     required this.emailToko,
     required this.deskripsiToko,
+    required this.provinsiToko,
+    required this.kotaToko,
   });
 
   factory TokoModel.fromJson(Map<String, dynamic> json) {
@@ -32,6 +37,49 @@ class TokoModel {
       nomorTeleponToko: json['NomorTeleponToko'],
       emailToko: json['emailToko'],
       deskripsiToko: json['deskripsiToko'],
+      provinsiToko: json['provinsiToko'],
+      kotaToko: json['kotaToko'],
     );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'userId': userId,
+      'fotoProfileToko': fotoProfileToko,
+      'namaToko': namaToko,
+      'kategoriToko': kategoriToko,
+      'alamatToko': alamatToko,
+      'nomorTeleponToko': nomorTeleponToko,
+      'emailToko': emailToko,
+      'deskripsiToko': deskripsiToko,
+      'provinsiToko': provinsiToko,
+      'kotaToko': kotaToko,
+      // 'jamOperasional': jamOperasional.map((e) => e.toJson()).toList(),
+      // 'fotoToko': fotoToko,
+    };
+  }
+}
+
+class JamOperasional {
+  final String hari;
+  final String jamBuka;
+  final String jamTutup;
+  final bool statusBuka;
+
+  JamOperasional({
+    required this.hari,
+    required this.jamBuka,
+    required this.jamTutup,
+    required this.statusBuka,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'hari': hari,
+      'jamBuka': jamBuka,
+      'jamTutup': jamTutup,
+      'statusBuka': statusBuka,
+    };
   }
 }
