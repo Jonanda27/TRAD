4b74b7a43d6e6a641a3031eb88f3de54a65a7bed
Jonanda27 <jonandapantas@gmail.com>
2024-09-26 18:23:11 +0700

    lupa

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      home: HalamanAwal(),
       onGenerateRoute: (settings) {
         // Handle named routes
         if (settings.name == '/tambahproduk') {
