675057fc640e6894a8cfdc29bcc3e475158f098c
LoneWanderer2403 <naufal.syafiq.tif22@polban.ac.id>
2024-10-11 22:39:03 +0700

    ubah sandi beres

--- frontend/lib/ubah_sandi.dart
+++ frontend/lib/ubah_sandi.dart
@@ -17,6 +17,11 @@ class _UbahSandiPageState extends State<UbahSandiPage> {
   bool _isNewPasswordVisible = false;
   bool _isConfirmPasswordVisible = false;
   bool _isOldPasswordSubmitted = false;
+  bool _allRequirementsMet = false;
+  bool _hasMinLength = false;
+  bool _hasUppercase = false;
+  bool _hasNumber = false;
+
   final _oldPasswordController = TextEditingController();
   final _newPasswordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
@@ -30,6 +35,13 @@ class _UbahSandiPageState extends State<UbahSandiPage> {
   }
 
   Future<void> _changePassword() async {
+    if (!_hasMinLength || !_hasUppercase || !_hasNumber) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Sandi baru tidak memenuhi persyaratan!')),
+      );
+      return;
+    }
+
     if (_newPasswordController.text != _confirmPasswordController.text) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Sandi baru dan konfirmasi sandi tidak cocok!')),
@@ -38,66 +50,66 @@ class _UbahSandiPageState extends State<UbahSandiPage> {
     }
 
     try {
-    final success = await ProfileService.changePassword(
-      _newPasswordController.text,
-    );
+      final success = await ProfileService.changePassword(
+        _newPasswordController.text,
+      );
 
-    if (success) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return Dialog(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(6),
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                AppBar(
-                  backgroundColor: const Color.fromRGBO(0, 84, 102, 1),
-                  title: Text(
-                    'Ubah Sandi Berhasil',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  automaticallyImplyLeading: false,
-                  centerTitle: true,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.vertical(
-                      top: Radius.circular(6),
+      if (success) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return Dialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(6),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  AppBar(
+                    backgroundColor: const Color.fromRGBO(0, 84, 102, 1),
+                    title: Text(
+                      'Ubah Sandi Berhasil',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    automaticallyImplyLeading: false,
+                    centerTitle: true,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(6),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 20),
-                Icon(Icons.check_circle, color: Colors.green, size: 100),
-                SizedBox(height: 20),
-                Text(
-                  'Sandi telah berhasil diperbaharui',
-                  style: TextStyle(fontSize: 16),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 20),
-                TextButton(
-                  child: Text('OK'),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(builder: (context) => EditProfilePage()),
-                    );
-                  },
-                ),
-                SizedBox(height: 10),
-              ],
-            ),
-          );
-        },
-      );
-    }} catch (e) {
+                  SizedBox(height: 20),
+                  Icon(Icons.check_circle, color: Colors.green, size: 100),
+                  SizedBox(height: 20),
+                  Text(
+                    'Sandi telah berhasil diperbaharui',
+                    style: TextStyle(fontSize: 16),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 20),
+                  TextButton(
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) => EditProfilePage()),
+                      );
+                    },
+                  ),
+                  SizedBox(height: 10),
+                ],
+              ),
+            );
+          },
+        );
+      }
+    } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Error: ${e.toString()}')),
       );
     }
   }
-
 
   Future<void> _checkOldPassword() async {
   if (_oldPasswordController.text.isEmpty) {
@@ -106,6 +118,15 @@ class _UbahSandiPageState extends State<UbahSandiPage> {
     );
     return;
   }
+
+//   void _checkPassword(String value) {
+//   setState(() {
+//     _hasMinLength = value.length >= 8;
+//     _hasUppercase = value.contains(RegExp(r'[A-Z]'));
+//     _hasNumber = value.contains(RegExp(r'[0-9]'));
+//     _allRequirementsMet = _hasMinLength && _hasUppercase && _hasNumber;
+//   });
+// }
 
   try { // Assuming you have this method
     final success = await ProfileService.checkOldPassword( _oldPasswordController.text);
@@ -124,6 +145,33 @@ class _UbahSandiPageState extends State<UbahSandiPage> {
       SnackBar(content: Text('Error: ${e.toString()}')),
     );
   }
+}
+
+void _checkPassword(String value) {
+  setState(() {
+    _hasMinLength = value.length >= 8;
+    _hasUppercase = value.contains(RegExp(r'[A-Z]'));
+    _hasNumber = value.contains(RegExp(r'[0-9]'));
+    _allRequirementsMet = _hasMinLength && _hasUppercase && _hasNumber;
+  });
+}
+
+
+  Widget _buildPasswordRequirement(String text, bool isMet) {
+  return Padding(
+    padding: const EdgeInsets.only(bottom: 8.0),
+    child: Row(
+      children: [
+        Icon(
+          isMet ? Icons.check_circle : Icons.cancel,
+          color: isMet ? Colors.green : Colors.red,
+          size: 16,
+        ),
+        SizedBox(width: 8),
+        Text(text, style: TextStyle(fontSize: 14)),
+      ],
+    ),
+  );
 }
 
 
@@ -198,28 +246,39 @@ class _UbahSandiPageState extends State<UbahSandiPage> {
             ),
             SizedBox(height: 10),
             TextField(
-              controller: _newPasswordController,
-              obscureText: !_isNewPasswordVisible,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                // labelText: 'Sandi Baru',
-                hintText: 'Sandi Baru',
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _isNewPasswordVisible
-                        ? Icons.visibility
-                        : Icons.visibility_off,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _isNewPasswordVisible = !_isNewPasswordVisible;
-                    });
-                  },
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            TextField(
+  controller: _newPasswordController,
+  obscureText: !_isNewPasswordVisible,
+  onChanged: _checkPassword,
+  decoration: InputDecoration(
+    border: OutlineInputBorder(),
+    hintText: 'Sandi Baru',
+    suffixIcon: IconButton(
+      icon: Icon(
+        _isNewPasswordVisible ? Icons.visibility : Icons.visibility_off,
+      ),
+      onPressed: () {
+        setState(() {
+          _isNewPasswordVisible = !_isNewPasswordVisible;
+        });
+      },
+    ),
+  ),
+),
+SizedBox(height: 10),
+Visibility(
+  visible: !_allRequirementsMet,
+  child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      _buildPasswordRequirement('Butuh minimal 8 Karakter', _hasMinLength),
+      _buildPasswordRequirement('Memiliki 1 Huruf Kapital', _hasUppercase),
+      _buildPasswordRequirement('Mengandung minimal 1 angka', _hasNumber),
+    ],
+  ),
+),
+SizedBox(height: 20),
+        
+              TextField(
               controller: _confirmPasswordController,
               obscureText: !_isConfirmPasswordVisible,
               decoration: InputDecoration(
